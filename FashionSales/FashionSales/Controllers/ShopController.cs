f973520daee77250b42095fe95f28854e9515dae
Hieu Pham-origin <hieupham112003@gmail.com>
2023-09-30 20:34:40 +0700

    Design FE

---
+++ FashionSales/FashionSales/Controllers/ShopController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FashionSales.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Shop()
+        {
+            return View();
+        }
+    }
+}
