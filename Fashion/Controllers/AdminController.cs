f73953a98031a9eb4d618ad9e13ce39c0849c8e2
Phú Phan Hoàng <52100086@student.tdtu.edu.vn>
2023-10-08 21:48:43 +0700

    update

---
+++ Fashion/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fashion.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Sales()
+        {
+            return View();
+        }
+
+        public IActionResult Invoice()
+        {
+            return View();
+        }
+        public IActionResult Invoice_Details()
+        {
+            return View();
+        }
+    }
+}
---
+++ Fashion/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Fashion.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Fashion.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+            
+        public IActionResult Shop()
+        {
+            return View();
+        }
+
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Blog()
+        {
+            return View();
+        }
+    }
+}
