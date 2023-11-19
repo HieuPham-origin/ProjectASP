23390efcef39c3de09f4825f95734a8169f4f4fa
Hieu Pham-origin <hieupham112003@gmail.com>
2023-11-20 00:31:06 +0700

    modify admin UI

--- Fashion/Controllers/UserController.cs
+++ Fashion/Controllers/UserController.cs
@@ -39,7 +39,10 @@ namespace Fashion.Controllers
                     string role = GetUserRole(email);
                     if (role == "admin")
                     {
-                        return RedirectToAction("Sales", "Admin");
+						HttpContext.Session.SetString("CustomerId", user.CustomerID.ToString());
+						HttpContext.Session.SetString("CustomerEmail", user.NormalizedEmail);
+						HttpContext.Session.SetString("CustomerLastName", user.LastName);
+						return RedirectToAction("Sales", "Admin");
                     }
                     else if (role == "user")
                     {
